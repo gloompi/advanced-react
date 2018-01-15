5d5a88c6b1b0dd1bd53a0a58004d0c3ff7d63965
gloompi <gloompi@gmail.com>
2018-01-15 20:10:21 +0800

    initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount = () => {
+    console.log('mounted')
+  }
+  
+  render() {
+    return (
+      <div>Sook pizdec</div>
+    );
+  }
+}
+
+export default App;
